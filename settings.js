b4bc7810dbbc5ea7024cd324cc27c83fd77f30fd
hammou1 <110303828+hammou1@users.noreply.github.com>
2025-05-25 09:49:11 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 
 SESSION_ID: process.env.SESSION_ID === undefined ? 'v6pUTbwC#Lffy41nDdI_CgPVDavOxb6LgduNZB6ClzrSU3rQcu50' : process.env.SESSION_ID,
-PREFIX: process.env.PREFIX || '.' ,
+PREFIX: process.env.PREFIX || '𝐒𝐔𝐋𝐀-𝐌𝐃=BzklRYDB#N8cJU7lE-KkAVZ1To3J3h5CYhMEoUP2i67kPyya9QGA' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
